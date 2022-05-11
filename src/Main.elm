7796deb6bb8ef46dd923f8f8821a31e78779523f
Anthony Suárez <neolight1010@gmail.com>
2022-05-10 21:38:06 -0500

    Implement authBoxView.

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,15 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, blockquote, button, div, h2, p, text)
-import Html.Attributes exposing (class)
+import Debug exposing (toString)
+import Html exposing (Html, blockquote, button, div, h2, h3, input, label, p, text)
+import Html.Attributes exposing (class, for, id, type_, value)
 import Html.Events exposing (onClick)
 import Http
 import Json.Decode
 import Json.Encode
 import Url.Builder
-import Debug exposing (toString)
+import Html.Events exposing (onInput)
 
 
 main : Program () Model Msg
@@ -34,6 +35,9 @@ type Msg
     = GetQuote
     | GotQuote (Result Http.Error String)
     | AuthedUser (Result Http.Error String)
+    | SetUsername String
+    | SetPassword String
+    | ClickRegisterUser
 
 
 init : () -> ( Model, Cmd Msg )
@@ -53,9 +57,61 @@ update msg model =
         AuthedUser result ->
             ( authedUser result model, Cmd.none )
 
+        _ ->
+            ( model, Cmd.none )
+
 
 view : Model -> Html Msg
 view model =
+    let
+        isLoggedIn : Bool
+        isLoggedIn =
+            not <| String.isEmpty model.token
+
+        authBoxView =
+            let
+                showErrorClass : String
+                showErrorClass =
+                    if String.isEmpty model.errorMsg then
+                        "hidden"
+
+                    else
+                        ""
+
+                greeting : String
+                greeting =
+                    "Hello, " ++ model.username ++ "!"
+            in
+            if isLoggedIn then
+                div [ id "greeting" ]
+                    [ h3 [ class "text-center" ] [ text greeting ]
+                    , p [ class "text-center" ] [ text "You have super-secret access to protected quotes." ]
+                    ]
+
+            else
+                div [ id "form" ]
+                    [ h2 [ class "text-center" ] [ text "Log In or Register" ]
+                    , div [ class showErrorClass ]
+                        [ div [ class "alert alert-danger" ] [ text model.errorMsg ]
+                        ]
+                    , div [ class "form-group-row" ]
+                        [ div [ class "col-md-offset-2 col-md-8" ]
+                            [ label [ for "username" ] [ text "Username:" ]
+                            , input [ id "username", type_ "text", class "form-control", value model.username, onInput SetUsername ] []
+                            ]
+                        ]
+                    , div [ class "form-group-row" ]
+                        [ div [ class "col-md-offset-2 col-md-8" ]
+                            [ label [ for "password" ] [ text "Password:" ]
+                            , input [ id "password", type_ "password", class "form-control", value model.password, onInput SetPassword ] []
+                            ]
+                        ]
+                    , div [ class "text-center"]
+                        [ button [class "btn btn-link", onClick ClickRegisterUser] [text "Register"]
+
+                            ]
+                    ]
+    in
     div [ class "container" ]
         [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
         , p [ class "text-center" ]
@@ -63,6 +119,9 @@ view model =
             ]
         , blockquote []
             [ p [] [ text model.quote ]
+            ]
+        , div [ class "jumbotron text-left" ]
+            [ authBoxView
             ]
         ]
 
